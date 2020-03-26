fe763857d55913f5e38c51eaa1cf8ad350cfe206
Arturo <caghp94@gmail.com>
2020-03-25 22:26:23 -0500

    base config

---
+++ lib/src/config/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final AppColors _appColors = AppColors._internal();
+
+  factory AppColors() {
+    return _appColors;
+  }
+
+  AppColors._internal();
+
+  static const int primary = 0xFFe20030;
+  static const int opaqueSurface = 0xFFf4f9fc;
+
+  static const MaterialColor primaryColor = MaterialColor(primary, <int, Color>{
+    40: Color(0xFFea466a),
+    60: Color(0xFFe73158),
+    80: Color(0xFFe51a46),
+    100: Color(primary),
+    120: Color(0xFFd6002d)
+  });
+
+  static const MaterialColor opaqueSurfaceColor =
+      MaterialColor(opaqueSurface, <int, Color>{100: Color(opaqueSurface)});
+}
---
+++ lib/src/config/routes.dart
@@ -0,0 +1,23 @@
+import 'package:app_5las/src/modules/auth/pages/login_page.dart';
+import 'package:app_5las/src/modules/splash/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class Router {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    //Page builders
+    MaterialPageRoute createMaterialRoute(Widget page, RouteSettings settings) {
+      return MaterialPageRoute(builder: (_) => page, settings: settings);
+    }
+
+    switch (settings.name) {
+      case loginRoute:
+        createMaterialRoute(LoginPage(), settings); break;
+      case splashRoute:
+        createMaterialRoute(SplashPage(), settings); break;
+    }
+  }
+
+  //Page strings
+  static const String loginRoute = 'login';
+  static const String splashRoute = 'splash';
+}
